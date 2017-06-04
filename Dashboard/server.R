320cd3b138dbcda3da78046af4b1eb3a113e2ddd
mpjimenez <mpjimene@calpoly.edu>
2017-06-04 08:58:24 -0700

    fixed the tabs on Line Chart

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -5,7 +5,7 @@ library(leaflet)
 
 
 function(input, output) {
-   data <- read.csv('../Crime/CSV/01_crime_in_the_united_states_by_volume_and_rate_per_100000_inhabitants_1996-2015.csv')
+   data <- read.csv('../Data/CSV/01_crime_in_the_united_states_by_volume_and_rate_per_100000_inhabitants_1996-2015.csv')
    
    output$lineChart <- renderPlot({
       ggplot(data, aes(x=input$choice, y=data[ , crimerate()], group=1),xlim=input$range) + 
--- Dashboard/ui.R
+++ Dashboard/ui.R
@@ -11,7 +11,7 @@ dashboardPage(
    dashboardSidebar(
       sidebarMenu(
          menuItem("Line Chart", tabName = "Line_Chart", icon = icon("line-chart")),
-         menuItem("Map", tabName = "widgets", icon = icon("map"))
+         menuItem("Map", tabName = "Map", icon = icon("map"))
       )
    ),
    
@@ -27,19 +27,21 @@ dashboardPage(
                box(selectizeInput('choice', 'Violent Crimes Committed', 
                                       choices = c('Murder'=1, 'Rape'=2, 'Aggravated Assult'=3, 'Robbery'=4),
                                       multiple = TRUE, options = list(maxItems = 4))
-               ),
-               
-               fluidRow(
-                  column(12,
-                     box(title='Rates of Violent Crime', status='danger', solidHeader = TRUE,
-                                          collapsible = TRUE, plotOutput('LineChart', height = 250)))
                )
             ),
+               
+            fluidRow(
+               column(12,
+                      box(title='Rates of Violent Crime', status='danger', solidHeader = TRUE,
+                          collapsible = TRUE, plotOutput('LineChart', height = 250))
+               )
+            )
+         ),
             
          # Second tab content
          tabItem(tabName = "Map", h2("Map of United States Using Leaflet"))
          
-         )
       )
    )
-)   
+)
+   
