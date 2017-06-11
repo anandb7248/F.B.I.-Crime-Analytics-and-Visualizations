4ae20bc978498ca48907a829bf008a49cb801e5c
mpjimenez <mpjimene@calpoly.edu>
2017-06-10 21:31:15 -0700

    cleaned up server.R by deleting commented out code

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -12,36 +12,25 @@ library(maps)
 
 
 function(input, output) {
-  us_crime <- read.csv('01_crime_in_the_united_states_1996-2015.csv')
-  data <- read.csv("2015_crimerates_by_state.csv",stringsAsFactors = F) #used to output choropleth plot
+   us_crime <- read.csv('01_crime_in_the_united_states_1996-2015.csv')
+   data <- read.csv("2015_crimerates_by_state.csv",stringsAsFactors = F) #used to output choropleth plot
   
-  #us_crime <- us_crime[, -1]
-  #data <- data[, -1]
-  #choro <- read.csv('choro_state_crimerate_2015.csv')
-  
- 
    choices <- reactive({
-     return(paste(input$choice,collapse=", "))
+      return(paste(input$choice,collapse=", "))
    })
    
    
    output$LineChart <- renderPlot({
-     years <- subset(us_crime, select = c("year",input$choice))
-     mdf <- melt(years, id.vars="year")
-     g<-ggplot(mdf, aes(x=year,y=value,colour=variable)) + 
-       xlim(input$range) + labs(x = "Year", y = paste("Crime Rate per 100,000 Inhabitants")) + 
-       title(main = 'Violent Crime Rates in the U.S.') + geom_line()
-     print(g)
+      years <- subset(us_crime, select = c("year",input$choice))
+      mdf <- melt(years, id.vars="year")
+      g<-ggplot(mdf, aes(x=year,y=value,colour=variable)) + 
+      xlim(input$range) + labs(x = "Year", y = paste("Crime Rate per 100,000 Inhabitants")) + 
+      title(main = 'Violent Crime Rates in the U.S.') + geom_line()
+      print(g)
    })
    
-   #output$mymap <- renderLeaflet({
-      #mapStates = map('state', fill = TRUE, plot = FALSE)
-      #leaflet(states_shp) %>% 
-        #addTiles() %>%
-         #addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
-   #})
-   
    states_shp <- readOGR(dsn="./states_shp",layer = "states")
+   
    # Remove district of columbia
    states_shp <- states_shp[!grepl("District of Columbia", states_shp$STATE_NAME), ]
    
@@ -55,16 +44,16 @@ function(input, output) {
       }
    }
    
-   STATE_FIPS[8] <- "10"
-   STATE_FIPS[23] <- "27"
-   
-   #Merge fips with dataframe of crime rates
-   data <- cbind(data,STATE_FIPS)
    # For some reason Minnesota and Delaware have na's as fip.
    # I then checked the fips function, for fips(delaware) it returned NA.
    # for fips(deleware), which is missplelled, it returned 10, which is the correct fip for Delaware.
    # So the function fip has implementation problems.
    # So for minnesota and delaware I am placing fip code manually.
+   STATE_FIPS[8] <- "10"
+   STATE_FIPS[23] <- "27"
+   
+   #Merge fips with dataframe of crime rates
+   data <- cbind(data,STATE_FIPS)
    
    #data$STATE_FIPS <- factor(data$STATE_FIPS)
    states <- merge(states_shp, data, by="STATE_FIPS",all=TRUE)
